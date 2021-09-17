02984dfe7faa2fde54c4392b7335150cff599fed
MatheusWilliam31 <mwsl.loose@hotmail.com>
2021-09-17 14:02:14 -0300

    Craindo CRUD Usuario

--- banca-noix/src/main/java/com/basis/bsb/bancanoix/dominio/Cargo.java
+++ banca-noix/src/main/java/com/basis/bsb/bancanoix/dominio/Cargo.java
@@ -4,18 +4,19 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "cargo")
-public class Cargo {
+public class Cargo implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
-    @Column (name = "titulo")
+    @Column(name = "titulo")
     private String titulo;
 }
