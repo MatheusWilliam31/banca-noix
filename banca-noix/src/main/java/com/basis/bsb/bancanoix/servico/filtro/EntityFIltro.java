dc8e126954310a25840f26dd181647d3bf0c9fa7
MatheusWilliam31 <mwsl.loose@hotmail.com>
2021-09-23 14:30:56 -0300

    Filtros

--- banca-noix/src/main/java/com/basis/bsb/bancanoix/servico/filtro/EntityFIltro.java
+++ banca-noix/src/main/java/com/basis/bsb/bancanoix/servico/filtro/EntityFIltro.java
@@ -2,9 +2,7 @@ package com.basis.bsb.bancanoix.servico.filtro;
 
 import org.springframework.data.jpa.domain.Specification;
 
-public interface EntityFIltro <T> {
+public interface EntityFIltro<T> {
 
-    Specification<T>  filter (
-
-    );
+    Specification<T> filter();
 }
