3f0af103f6ddcb5ff6ad7731095d206b17a9d467
macalsr <mmariasribeiro@gmail.com>
2021-09-30 13:18:42 -0300

    atualizando rotas usuario

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
+import { DiarioErrosComponent } from './components/diario-erros/diario-erros.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginSuccessComponent } from '@nuvem/angular-base';
-import { UsuarioComponent } from './components/usuario/usuario.component';
 
 const routes: Routes = [
   { path: 'diario-erros', component: DiarioErrosComponent, data: { breadcrumb: 'Diário de Erros' } },
   { path: 'login-success', component: LoginSuccessComponent },
-  { path: '', component: UsuarioComponent },
-  { path: '', loadChildren: './module/usuario/UsuarioRoutingModule'}
+  { path: 'usuario', loadChildren: './module/usuario.module#UsuarioModule'}
 
 ];
 
