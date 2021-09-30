beb27e06d82a19afe6cdc9aa717bf409cecf9643
macalsr <mmariasribeiro@gmail.com>
2021-09-30 13:36:49 -0300

    atualizando minha branch

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'showcase'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('showcase');
+    expect(app.topbarItemClick).toEqual('showcase');
   });
 
   it('should render title', () => {
--- frontend/src/app/module/usuario-routing.module.ts
+++ frontend/src/app/module/usuario-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 const routes: Routes = [
-  { path: '', component:UsuarioComponent }
+  { path: '', component:UsuarioComponent}
 ];
 
 @NgModule({
