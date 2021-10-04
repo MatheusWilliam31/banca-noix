cd1a733505155908792ad0ddd04bd96a343dcda4
Brendon18 <brendon1872@gmail.com>
2021-10-04 13:30:12 -0300

    cadastro de usuario

--- frontend/src/app/components/cadastro-usuario/cadastro-usuario.component.ts
+++ frontend/src/app/components/cadastro-usuario/cadastro-usuario.component.ts
@@ -1,15 +1,87 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MessageService, SelectItem } from 'primeng';
+import { Cargo } from 'src/app/model/cargo';
+import { CadastroUsuarioService } from 'src/app/service/cadastro-usuario.service';
+import { CargoService } from 'src/app/service/cargo.sevice';
 
 @Component({
   selector: 'app-cadastro-usuario',
   templateUrl: './cadastro-usuario.component.html',
   styleUrls: ['./cadastro-usuario.component.css']
 })
-export class CadastroUsuarioComponent implements OnInit {
 
-  constructor() { }
+export class CadastroUsuarioComponent implements OnInit{
+
+    @Input() tituloInterno: string = '';
+    @Input() usuario: UsuarioApp = null;
+
+  public form: FormGroup;
+  public formBuilder: FormBuilder = new FormBuilder;
+  public cargos: SelectItem[] = [];
+
+  constructor (
+      private messageService: MessageService,
+      private usuarioSerice: CadastroUsuarioService,
+      private cargoService: CargoService
+    ){    }
 
   ngOnInit(): void {
+      this.criarFormulario();
+      this.obterCargos();
+  }
+
+  public criarFormulario():void {
+      this.form = this.formBuilder.group({
+          id: [null],
+          nome: ['', Validators.required],
+          cpf: ['', Validators.required],
+          email: ['', Validators.required],
+          cargo: [null, Validators.required],
+          dataNascimento: ['', Validators.required],
+          status: [true, Validators.required],
+          telefone: ['']
+      })
+  }
+
+  public obterCargos():void {
+      this.cargoService.listar().subscribe((cargos : SelectItem [] => this.cargos = Cargo()));
+}
+
+    public formatarDataformulario(): Date {
+        let dataSplit: string[] = (this.form.get('dataNascimento').value as String).split
+    }
+
+  public submit():void {
+      if(this.form.valid){
+          FuncoesUtil.messagemErro(this.messageService, MessageUtils);
+          return;
+      }
+      this.form.get('dataNascimento').setValue(this.formatarDataFormulario());
+      this.form.get('cargo').setValue({value:this.form.get('cargo').value})
+      this.usuarioSerice.criar(this.form.getRawValue()).subscribe{
+          (usuarioApp : UsuarioApp) => FuncoesUtil.messagemSucesso(this.messagemService,
+          () =>FuncoesUtil.messagemErro(this.messageService, MessageUtils))
+      }
+  }
+
+  public inicializarCabecalho(): void {
+      if(this.tituloInterno.length == 0){
+          this.tituloInterno = "Cadastrar Usuário";
+      }
+  }
+
+  public preencherFormulario(): void{
+      if(this.usuario){
+          FormUtils.mudarValorCampoForm(this.form, 'id', this.usuario.id);
+          FormUtils.mudarValorCampoForm(this.form, 'cargo', this.usuario.id);
+          FormUtils.mudarValorCampoForm(this.form, 'cpf', this.usuario.id);
+          FormUtils.mudarValorCampoForm(this.form, 'dataNascimento', this.usuario.id);
+          FormUtils.mudarValorCampoForm(this.form, 'email', this.usuario.id);
+          FormUtils.mudarValorCampoForm(this.form, 'nome', this.usuario.id);
+          FormUtils.mudarValorCampoForm(this.form, 'status', this.usuario.id);
+          FormUtils.mudarValorCampoForm(this.form, 'telefone', this.usuario.id);
+      }
   }
 
 }
---
+++ frontend/src/app/service/cargo.sevice.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { SelectItem } from 'primeng';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CargoService { 
+    public readonly BASE_URL: string = '/cargos';
+
+    constructor(private http: HttpClient){}
+
+    public listar(): Observable<SelectItem[]> {
+        return this.http.get<SelectItem[]>(`${environment.apiUrl}${this.BASE_URL}`);
+    }
+}
